d8aecd18a45d775b52f10215bc7ccafce3c966e9
Michael de Lang <kingoipo@gmail.com>
2017-05-19 21:52:32 +0200

    Add compile_external script

---
+++ compile_external.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CPUS="$( grep -c ^processor /proc/cpuinfo )"
+cd $DIR/external/common
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo .
+make -j$CPUS
+cd $DIR/external/libpqxx
+./configure --disable-documentation
+make -j$CPUS
+cd $DIR/external/librdkafka
+./configure
+make -j$CPUS
+cd $DIR
